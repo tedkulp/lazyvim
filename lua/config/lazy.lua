d2c0354eccdd436c6e3bdbc70ec1449b2881c518
Ted Kulp <ted@tedkulp.com>
2023-09-27 08:11:13 -0400

    feat: enable some plugin sets

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,11 +10,17 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
