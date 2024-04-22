b86ff0b7efe2b2550ea5c2b0e35c61e5d3445cde
Ted Kulp <ted@tedkulp.com>
2024-04-22 06:46:04 -0400

    chore: updates

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,17 +3,18 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
-      "zbirenbaum/copilot-cmp",
+      -- "zbirenbaum/copilot-cmp",
     },
     opts = function(_, opts)
       local cmp = require("cmp")
-      -- local copilot = require("copilot.suggestion")
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
 
+      -- -- local copilot = require("copilot.suggestion")
+      -- local has_words_before = function()
+      --   unpack = unpack or table.unpack
+      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
+      --
       opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       opts.mapping["<S-CR>"] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Replace,
@@ -21,29 +22,29 @@ return {
       }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       opts.mapping["<C-j>"] = cmp.mapping.select_next_item()
       opts.mapping["<C-k>"] = cmp.mapping.select_prev_item()
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        -- if copilot.is_visible() then
-        --   copilot.accept()
-        -- elseif cmp.visible() then
-        if cmp.visible() then
-          cmp.select_next_item()
-        -- elseif luasnip.expand_or_jumpable() then
-        --   luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" })
-      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          --   luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" })
+      -- opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      --   -- if copilot.is_visible() then
+      --   --   copilot.accept()
+      --   -- elseif cmp.visible() then
+      --   if cmp.visible() then
+      --     cmp.select_next_item()
+      --   -- elseif luasnip.expand_or_jumpable() then
+      --   --   luasnip.expand_or_jump()
+      --   elseif has_words_before() then
+      --     cmp.complete()
+      --   else
+      --     fallback()
+      --   end
+      -- end, { "i", "s" })
+      -- opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_prev_item()
+      --     -- elseif luasnip.jumpable(-1) then
+      --     --   luasnip.jump(-1)
+      --   else
+      --     fallback()
+      --   end
+      -- end, { "i", "s" })
 
       table.insert(opts.sources, { name = "emoji" })
     end,
--- lua/plugins/tardis.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "fredeeb/tardis.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = true,
-  keys = { { "<leader>gt", "<cmd>Tardis<cr>", desc = "Tardis (git history)" } },
-}
