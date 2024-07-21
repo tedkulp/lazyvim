1e13a9e610e24efd8b108875ccb7bb833d0dc22f
Ted Kulp <ted@tedkulp.com>
2024-07-21 19:18:52 -0400

    chore: changes

---
+++ lua/plugins/edgy.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/edgy.nvim",
+  opts = function(_, opts)
+    opts.animate = {
+      enabled = false,
+    }
+  end,
+}
---
+++ lua/plugins/suda.lua
@@ -0,0 +1,8 @@
+return {
+  "lambdalisue/vim-suda",
+  init = function()
+    vim.cmd([[
+      let g:suda_smart_edit = 1
+    ]])
+  end,
+}
