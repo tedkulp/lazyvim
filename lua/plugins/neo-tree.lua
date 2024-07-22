1736871d3cc8067569fde85c88df7a1c6d23efb1
Ted Kulp <ted@allocate.co>
2024-07-22 08:34:14 -0400

    feat: added transparent.nvim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   optional = true,
   opts = {
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
     filesystem = {
       filtered_items = {
         visible = false,
